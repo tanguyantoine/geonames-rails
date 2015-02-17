5823f5b723780ef6a1dbe4501ef91dd112407291
jakikiller <antoine.tanguy35@gmail.com>
2015-02-17 19:22:03 +0100

    Update(

--- app/models/geonames_admin1.rb
+++ app/models/geonames_admin1.rb
@@ -1,4 +1,8 @@
 class GeonamesAdmin1 < GeonamesFeature
+  def admin2
+    GeonamesAdmin2.where(country_code: country_code, admin1_code: admin1_code)
+  end
+
   def cities
     country.cities.where(admin1_code: admin1_code )
   end
